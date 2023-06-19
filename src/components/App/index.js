af26ee6c5ad47e6400d1df4682dc39fd64feb9d1
AnoukFerrand <anoukfrd@gmail.com>
2023-06-19 12:42:19 +0200

    Add Footer, JSX in App Component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,21 @@
 import "./styles.scss";
 
+import Navbar from "../Navbar";
+import Playmat from "../Playmat";
+import Footer from "../Footer";
+
 function App() {
   return (
     <div className="app">
-      <p>App Here</p>
+      <header className="header">
+        <Navbar />
+      </header>
+      <main className="playmat">
+        <Playmat />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>footer Here</p>
+    </div>
+  );
+}
+
+export default Footer;
