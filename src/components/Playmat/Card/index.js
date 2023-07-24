995b00d99f06f534f6bb90bfc41391a02c7fdbad
AnoukFerrand <anoukfrd@gmail.com>
2023-07-24 13:58:59 +0200

    useless comments removed

--- src/components/Playmat/Card/index.js
+++ src/components/Playmat/Card/index.js
@@ -1,9 +1,8 @@
 import "./styles.scss";
-import backCard from "../../../../public/images/00.png";
 
 import PropTypes from "prop-types";
 
-// Here we recieved the card needed and found with the map
+// Here we recieved the card needed and found with "map"
 // and we put in <img> informations needed from the card card.img, card.name
 
 function Card({ card, index, clickHandler }) {
@@ -11,36 +10,26 @@ function Card({ card, index, clickHandler }) {
     <div className="cardsFlip">
       <div className="cardsFront">
         <div
-          // ADD A TERNAIRE TO CHANGE CARDS BACK
-          // NOT ENOUGH, "UNMATCH" "MATCH" CLASSNAME DOESN'T APPEAR ANYMORE
-          // card.status ? "active" : "cardsBack"
           className={`card ${card.status}`}
           onClick={() => clickHandler(index)}
         >
           <img src={card.img} alt={card.name} />
         </div>
-
-        {/* // ADD A BACK IMAGE : */}
       </div>
-      {/* <div className="cardsBack">
-        <div className={`card ${card.status}`}>
-          <img src="/images/00.png" alt="backCard" className="frontImage" />
-        </div>
-      </div> */}
     </div>
   );
 }
 
 // PropTypes needed cause of the use of es-lint, but not necessary in this project
-
-// Card.propTypes = {
-//   card: PropTypes.shape({
-//     img: PropTypes.string.isRequired,
-//     name: PropTypes.string.isRequired,
-//   }),
-// };
-// Card.defaultProps = {
-//   card: "",
-// };
+Card.propTypes = {
+  card: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }),
+};
+Card.defaultProps = {
+  card: "",
+};
 
 export default Card;
